d6e21ef9b2110a680dbdeb5426bd6839df8d4e20
Vasiliki Pasalidi-Chantzi <van3pasa@hotmail.com>
2021-10-10 00:31:31 +0100

    Add full css & app fully functional

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,36 +1,17 @@
-import { sign } from "crypto";
-import { useState, useContext } from "react";
-import { Link } from "react-router-dom";
-import swal from "sweetalert";
-import { projectAuth } from "../firebase/config";
-import { AuthContext } from "./Authorization";
+import { useState } from "react";
 import ImageList from "./ImageList";
 import { Intro } from "./Intro";
 import Modal from "./Modal";
 import UploadPhoto from "./UploadPhoto";
 
 
-
 function Main(): JSX.Element {
   const [selectedImage, setSelectedImage] =useState<string|undefined>(undefined)
   
-  const signOut = () => {
-    projectAuth.signOut()
-    swal("Signed out successfully!", "Going back to the login page", "success")
-  }
-
-
-  const user = useContext(AuthContext)
    return (
      <>
     <div className="App">
-    <Intro/>
-    {user && <div className="welcome">
-      Welcome, {user.displayName || "user"}! 
-      <small>{user.email}</small>
-      </div>}
- 
-    <Link to="/signin"><button className="sing-out-btn" onClick={signOut}> Sign out</button> </Link>
+    <Intro/> 
     <UploadPhoto />
     <ImageList setSelectedImage={setSelectedImage}/>
     { selectedImage && <Modal selectedImage={selectedImage} setSelectedImage={setSelectedImage}/>}
